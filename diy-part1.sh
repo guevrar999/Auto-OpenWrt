e41182e400cb014669cc1eb0a1f36af89f765ca3
guevrar999 <58546559+guevrar999@users.noreply.github.com>
2021-11-29 00:27:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,6 +23,7 @@
 
 ####### 科学上网插件 #######
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 
 ####### darkmatter主题 #######
